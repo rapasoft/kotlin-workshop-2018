b5ee6f8fdee848ba29274ccdcf64561f43b3fbdd
Pavol Rajzak <pavol.rajzak@itera.no>
2018-10-12 20:36:40 +0200

    Koans - assignements (with solution)

---
+++ src/koans/collections.kt
@@ -0,0 +1,80 @@
+package koans
+
+/**
+ * Implement these methods for lists of customers.
+ */
+
+/**
+ * This function returns the sum of paid invoices for customer and month.
+ *
+ * Return a Map, where key is a customer name and the value is the sum.
+ */
+fun List<Customer>.calculateRevenuePerCustomerAndMonth(month: Int): Map<String, Int> =
+    this.map { customer ->
+        Pair(
+            customer.name,
+            customer.projects
+                .flatMap { it.invoices }
+                .filter { it.paid }
+                .filter { it.invoicingPeriod.month == month }
+                .sumBy { it.price })
+    }.toMap()
+
+/**
+ * This function returns the project, which has the highest sum of all paid invoices
+ */
+fun List<Customer>.findMostProfitableProject(): Project? =
+    this.flatMap { it.projects }
+        .maxBy { project ->
+            project.invoices
+                .filter { it.paid }
+                .sumBy { it.price }
+        }
+
+
+class TestData {
+    companion object {
+        val customers = listOf(
+            Customer("Westside Chocolate Factory", listOf(
+                Project("Chocolate Quality Improvement", listOf(
+                    Invoice(14_000_000, false, InvoicingPeriod(10, 2018)),
+                    Invoice(13_000_000, true, InvoicingPeriod(9, 2018)),
+                    Invoice(10_000_000, true, InvoicingPeriod(8, 2018)),
+                    Invoice(4_000_000, true, InvoicingPeriod(7, 2018))
+                )),
+                Project("Unnamed Hot Cocoa Project", listOf(
+                    Invoice(45_000_000, false, InvoicingPeriod(10, 2018)),
+                    Invoice(40_000_000, false, InvoicingPeriod(9, 2018))
+                ))
+            )),
+            Customer("Granny's little bakery", listOf(
+                Project("European Market Expansion", listOf(
+                    Invoice(314_000_000, false, InvoicingPeriod(10, 2018)),
+                    Invoice(300_000_000, true, InvoicingPeriod(9, 2018)),
+                    Invoice(430_000_000, true, InvoicingPeriod(8, 2018))
+                ))
+            )),
+            Customer("Arnie's Pizza Palace", listOf(
+                Project("Oven maintenance", listOf(
+                    Invoice(13_000, true, InvoicingPeriod(10, 2018)),
+                    Invoice(11_000, true, InvoicingPeriod(9, 2018)),
+                    Invoice(13_000, true, InvoicingPeriod(8, 2018))
+                )),
+                Project("Delivery car fleet upgrade", listOf(
+                    Invoice(250_000, false, InvoicingPeriod(10, 2018)),
+                    Invoice(450_000, true, InvoicingPeriod(9, 2018)),
+                    Invoice(133_000, true, InvoicingPeriod(8, 2018))
+                ))
+            ))
+        )
+    }
+}
+
+fun main(args: Array<String>) {
+    println(0 == TestData.customers.calculateRevenuePerCustomerAndMonth(10)["Westside Chocolate Factory"])
+    println(13_000_000 == TestData.customers.calculateRevenuePerCustomerAndMonth(9)["Westside Chocolate Factory"])
+    println(10_000_000 == TestData.customers.calculateRevenuePerCustomerAndMonth(8)["Westside Chocolate Factory"])
+    println(11_000 + 450_000 == TestData.customers.calculateRevenuePerCustomerAndMonth(9)["Arnie's Pizza Palace"])
+    println(430_000_000 == TestData.customers.calculateRevenuePerCustomerAndMonth(8)["Granny's little bakery"])
+    println("European Market Expansion" == TestData.customers.findMostProfitableProject()!!.name)
+}
---
+++ src/koans/models.kt
@@ -0,0 +1,54 @@
+package koans
+
+/**
+ * First assignment
+ *
+ * Implement correctly methods and interfaces required for the code in `main` method to compile.
+ *
+ *  1. Implement `kotlin.Comparable<InvoicingPeriod>` that will compare the periods (by month/year time)
+ *  2. Implement `inc()` operator in order to increment period by one month
+ *  3. Lastly, implement simple validation using `init {}` intializer that will throw IllegalArgumentException when
+ *     parameter month is not within range of <1, 12> and year is less than 1970 :)
+ *
+ *  Try to "think" in Kotlin and use as less constructs as needed (e.g. make us of type inference, lambdas, etc.)
+ *
+ *  You can use these parts of documentation:
+ *  - https://kotlinlang.org/docs/reference/interfaces.html#implementing-interfaces
+ *  - https://kotlinlang.org/docs/reference/operator-overloading.html#increments-and-decrements
+ *  - https://kotlinlang.org/api/latest/jvm/stdlib/kotlin/require.html#require
+ *
+ */
+
+data class InvoicingPeriod(val month: Int, val year: Int) : Comparable<InvoicingPeriod> {
+    override fun compareTo(other: InvoicingPeriod) = this.year - other.year + this.month - other.month
+    operator fun inc(): InvoicingPeriod = this.copy(
+        if (month < 12) {
+            month + 1
+        } else 1,
+        if (month == 12) {
+            year + 1
+        } else year
+    )
+
+    init {
+        require(month in 1 .. 12 && year > 1970) { "You must use valid month and year" }
+    }
+}
+
+data class Invoice(val price: Int, val paid: Boolean, val invoicingPeriod: InvoicingPeriod)
+data class Customer(val name: String, val projects: List<Project>)
+data class Project(val name: String, val invoices: List<Invoice>)
+
+fun main(args: Array<String>) {
+    println(InvoicingPeriod(1, 2018) < InvoicingPeriod(10, 2018))
+    println(InvoicingPeriod(1, 2018) == InvoicingPeriod(1, 2018))
+    println(InvoicingPeriod(10, 2018) > InvoicingPeriod(1, 2018))
+    println(InvoicingPeriod(5, 2018) in InvoicingPeriod(1, 2018)..InvoicingPeriod(12, 2018))
+    var start = InvoicingPeriod(12, 2018)
+    println(++start == InvoicingPeriod(1, 2019))
+    try {
+        println(InvoicingPeriod(-5, 2018))
+    } catch (illegalArgumentException: IllegalArgumentException) {
+        println("You've done it right!")
+    }
+}
